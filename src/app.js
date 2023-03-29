9c7711815ce5097ccba6e7b97384971e25936424
VictoriaRoshka <kawaii0109@mail.ru>
2023-03-29 13:29:17 +0300

    добавлен контейнер "нет результатов"

--- src/app.js
+++ src/app.js
@@ -49,12 +49,13 @@ function showTours(tours) {
         tour.image
     }" alt=""/></div>
     <div class="flex items-center justife-center">${location}</div>
-    <div class="flex justify-between px-2"><h1 class="font-bold text-xl text-amber-700">${
-        tour.hotelName
-    }</h1>
-  <div class="rating flex"><div class="items-center justify-center text-md font-medium text-justify text-gray-700">${
-      tour.rating
-  }</div></div></div>
+    <div class="flex justify-between px-2">
+    <h1 class="font-bold text-xl text-amber-700">${tour.hotelName}</h1>
+    <div class="flex">
+    <div class="items-center justify-center text-md font-medium text-justify text-gray-700">${
+        tour.rating}</div>
+        <div class="rating flex">
+    </div></div></div>
     <div class="flex px-2 gap-1 items-center">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
@@ -75,7 +76,7 @@ function showTours(tours) {
           
         <p class="text-lime-900 text-xl font-semibold">${tour.price}₽</p></div>
     <div class="flex gap-2 justify-between items-center py-4 px-2">
-        <button class="btn-primary">Подробнее</button>
+        <button class="btn-primary">Забронировать</button>
         <button><svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="w-8 h-8 hover:fill-lime-700">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
           </svg></button>
@@ -85,7 +86,7 @@ function showTours(tours) {
 }
 
 function filterTours(tours) {
-
+    const noResults = document.getElementById('no-results')
     const country = document.getElementById('select-country').value
     const rate = document.getElementById('select-rate').value
     const minPrice = document.getElementById('min-price').value
@@ -93,11 +94,13 @@ function filterTours(tours) {
 
     const filteredCountry = tours.filter((tour) => {
 
+
         if(country === "all-country"){
             return true
         } else {
            return tour.country === country
         }
+
      })
     
     const filteredRate = filteredCountry.filter((tour) => {
@@ -107,22 +110,31 @@ function filterTours(tours) {
      const filteredPrice = filteredRate.filter((tour)=> {
         if(tour.price >= minPrice && maxPrice >= tour.price){
             return true
+        }else if(minPrice < tour.price && maxPrice < tour.price){
+            return false
         }
+        
+            
+
         
      })
 
-     if(minPrice.length > 0, maxPrice.length > 0){
-        showTours(filteredPrice)
+   if(filteredPrice.length === 0){
+            noResults.style.display = "flex"
+        }else if(filteredRate.length === 0){
+            noResults.style.display = "flex"
 
-     }else{
-        showTours(filteredRate)
-     }
+        }else if(filteredCountry.length === 0){
+            noResults.style.display = "flex"
+        }
 
-     
-  
-
-
-
+        if(minPrice>0, maxPrice.length >0){
+            noResults.style.display = "none"
+            showTours(filteredPrice)
+    
+         }else{
+            noResults.style.display = "none"
+            showTours(filteredRate)}
 }
 
 
@@ -130,9 +142,6 @@ async function init() {
     const tours = await loadTours()
     showTours(tours)
 
-    searchTour.addEventListener('click', ()=> filterTours(tours))
-
-}
-
+    searchTour.addEventListener('click', ()=> filterTours(tours))}
 
 init()
