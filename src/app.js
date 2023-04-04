a31ae566aae18e87aad2038a287d20f2a0ec8bfc
VictoriaRoshka <kawaii0109@mail.ru>
2023-04-04 17:56:13 +0300

    в модальном окне показывает выбранный тур

--- src/app.js
+++ src/app.js
@@ -76,14 +76,50 @@ function showTours(tours) {
           
         <p class="text-lime-900 text-xl font-semibold">${tour.price}₽</p></div>
     <div class="flex gap-2 justify-between items-center py-4 px-2">
-        <button class="btn-primary">Забронировать</button>
+        <button class="btn-primary" id="btn-buyTour_${tour.id}">Забронировать</button>
         <button><svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="green" class="w-8 h-8 hover:fill-lime-700">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
           </svg></button>
           
     </div>`
     })
+
+    tours.forEach((tour) => {
+
+        const modalBox = document.getElementById("modal-box")
+        const orderTour = document.getElementById(`btn-buyTour_${tour.id}`)
+        const closeBtn = document.getElementById("closeModal")
+        const tourImage = document.getElementById("modal-box__tour-image")
+        const tourCountry = document.getElementById("modal-box__tour-country")
+        const tourHotel = document.getElementById("modal-box__tour-hotel")
+        const tourData = document.getElementById("modal-box__tour-data")
+        const tourPrice = document.getElementById("modal-box__tour-price")
+
+        orderTour.addEventListener('click', showModal)
+        closeBtn.addEventListener('click', closeModal)
+    
+        function showModal(){
+            
+            modalBox.style.display = 'flex'
+
+            tourImage.innerHTML = tour.image
+            tourCountry.innerHTML = tour.country
+            tourHotel.innerHTML = tour.hotelName
+            tourData.innerHTML = tour.data
+            tourPrice.innerHTML = tour.price
+
+
+    
+        }
+        function closeModal(){
+
+            modalBox.style.display = 'none'
+        }
+    
+    })
 }
+
+
 
 
 function filterTours(tours) {
